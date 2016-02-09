8d3b5b4060a13e61beffc2c39564a0d9818e3753
Santtu Pajukanta <santtu@pajukanta.fi>
2016-02-09 15:09:25 +0200

    optimized production build

--- app/Greeter.js
+++ app/Greeter.js
@@ -1,10 +1,11 @@
 import React, {Component} from 'react'
 import config from './config.json';
+import styles from './styles.css';
 
 class Greeter extends Component{
   render() {
     return (
-      <div>
+      <div className={styles.root}>
         {config.greetText}
       </div>
     );
